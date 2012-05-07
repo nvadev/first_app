cb90935d9e1fd7b83cc3c5727977b44bac883434
NVA <NVA@nva-mms.local>
2012-05-07 21:05:56 +0700

    Initial  commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '01168aa81157115b6a51a8eb0b504af4f4b630751d26106361679bfc46652c9766b3c8f7b6d0716cf02799dae2d854f74792e29817392745f897451f08f8b63a'
